fa19518e9777a785bbdd075789bf10d6e81c9f94
fanyoufd <fanyoufd@outbooks.com>
2020-03-18 08:42:01 +0800

    登录页面 基础框架搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,9 +2,21 @@
    <!-- 登录 -->
   <div class="container">
       <!-- 头部导航栏 左箭头left-arrow -->
-      <van-nav-bar title="登录"  left-arrow @click-left="$router.go(-1)">
-
-      </van-nav-bar>
+      <van-nav-bar title="登录"  left-arrow @click-left="$router.go(-1)"></van-nav-bar>
+      <!-- 登录模块 布局 -->
+      <van-cell-group>
+        <!-- 手机号 -->
+       <van-field v-model="value" placeholder="请输入手机号" label="手机号"></van-field>
+       <!-- 验证码 -->
+       <van-field v-model="value" placeholder="请输入验证码" label="验证码">
+         <!-- 验证码 按钮 -->
+         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+       </van-field>
+      </van-cell-group>
+        <!-- 登录按钮 -->
+        <div class="login-box">
+        <van-button type="info" block round size="small"> 登录 </van-button>
+        </div>
   </div>
 </template>
 
@@ -15,5 +27,7 @@ export default {
 </script>
 
 <style>
-
+.login-box{
+padding: 15px;
+}
 </style>
