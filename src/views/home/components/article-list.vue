41cafa5f7dad2a1b31d98a6e94c3115699acfbd4
fanyoufd <fanyoufd@outbooks.com>
2020-03-19 19:58:59 +0800

    文章列表 - 布局搭建

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -8,7 +8,38 @@
       <van-list v-model="uploading" :finished="finished" @load="onLoad" finished-text="数据加载完毕">
         <!-- 边框 -->
         <van-cell-group>
-          <van-cell :title="`标题${item}`" value="内容" v-for="item in list" :key="item"></van-cell>
+          <van-cell :title="`标题${item}`" value="内容" v-for="item in list" :key="item">
+             <!-- item.art_id 此时是一个大数字的对象 v-for 的key需要用字符串或者数字代理 -->
+          <van-cell v-for="item in articles" :key="item.art_id.toString()">
+            <!-- 放置元素 文章列表的循环项  无图  单图  三图 -->
+            <div class="article_item">
+              <!-- 标题 -->
+              <h3 class="van-ellipsis">{{ item.title }}</h3>
+              <!-- 根据当前的封面类型决定显示单图 三图 还是无图 -->
+              <!-- 三图图片 -->
+              <div class="img_box" v-if="item.cover.type === 3">
+                <!-- 图片组件用的是 vant的组件库中的图片组件 需要使用该组件 进行图片的懒加载 -->
+                <van-image class="w33" fit="cover" :src="item.cover.images[0]" />
+                <van-image class="w33" fit="cover" :src="item.cover.images[1]" />
+                <van-image class="w33" fit="cover" :src="item.cover.images[2]" />
+              </div>
+              <!-- 单图 暂时隐藏掉单图-->
+               <div class="img_box" v-if="item.cover.type === 1">
+                 <!-- 单图取第一个 -->
+                <van-image class="w100" fit="cover" :src="item.cover.images[0]" />
+              </div>
+              <!-- 作者信息 -->
+              <div class="info_box">
+                <span>名字</span>
+                <span>0评论</span>
+                <span>事件</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+          </van-cell>
+          </van-cell>
         </van-cell-group>
       </van-list>
     </van-pull-refresh>
@@ -55,5 +86,45 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
