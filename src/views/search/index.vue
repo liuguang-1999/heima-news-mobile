8f94f6dc2c9787760a91fcd2628049079e1f8df6
fanyoufd <fanyoufd@outbooks.com>
2020-03-23 09:07:22 +0800

    搜索关键字 和 历史记录及联想搜索

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -4,14 +4,15 @@
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
     <!-- 搜索组件 -->
-    <van-search  placeholder="请输入搜索关键词" shape="round" />
-    <van-cell-group class="suggest-box" >
+    <van-search  placeholder="请输入搜索关键词" shape="round"  v-model.trim="q" />
+    <!-- 联想内容 输入内容时 显示联想内容 -->
+    <van-cell-group class="suggest-box" v-if="q">
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
     </van-cell-group>
     <!-- 历史部分 收缩后的记录 会在这里显示 -->
-    <div class="history-box">
+    <div class="history-box" v-else>
       <div class="head">
         <span>历史记录</span>
         <van-icon name="delete"></van-icon>
@@ -28,7 +29,12 @@
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      q: '' // 关键字的数据
+    }
+  }
 }
 </script>
 
